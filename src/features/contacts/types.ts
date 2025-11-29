d92fd60fb833fd92b15fcd29cbaf7845121c7cc3
Ashok Kumar <ashok.kumar@revlitix.com>
2025-11-29 12:39:15 +0530

    add: create Contact interface for contact management

---
+++ src/features/contacts/types.ts
@@ -0,0 +1,10 @@
+export interface Contact {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+  addressLine1: string;
+  addressLine2?: string;
+  state: string;
+  pincode: string;
+}
