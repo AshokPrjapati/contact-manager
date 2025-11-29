1a221e6b19613fbd3321a727d5acd430af70c37e
Ashok Kumar <ashok.kumar@revlitix.com>
2025-11-29 13:05:59 +0530

    add: create Nav component and integrate it into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
+import Nav from "./components/Nav";
+
 function App() {
   return (
     <main>
-      <div
-        style={{
-          background: "linear-gradient(258.07deg, #07CDBE 0%, #006EFA 91.28%)",
-          color: "white",
-          fontSize: "24px",
-          fontWeight: "600",
-          letterSpacing: "0.5px",
-          height: "60px",
-          display: "flex",
-          alignItems: "center",
-          paddingLeft: "40px",
-        }}
-      >
-        <img width={138} height={32} src="/assets/logo.svg" alt="Logo" />
-      </div>
+      <Nav />
     </main>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,21 @@
+const Nav = () => {
+  return (
+    <div
+      style={{
+        background: "linear-gradient(258.07deg, #07CDBE 0%, #006EFA 91.28%)",
+        color: "white",
+        fontSize: "24px",
+        fontWeight: "600",
+        letterSpacing: "0.5px",
+        height: "60px",
+        display: "flex",
+        alignItems: "center",
+        paddingLeft: "40px",
+      }}
+    >
+      <img width={138} height={32} src="/assets/logo.svg" alt="Logo" />
+    </div>
+  );
+};
+
+export default Nav;
